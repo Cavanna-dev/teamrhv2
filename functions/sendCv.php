2a12c9e1aae32885b2c9cd001a45de0069163a00
Christophe <Christophe@WORKSTATION.home>
2015-11-15 21:19:48 +0100

    fixes client

--- functions/sendCv.php
+++ functions/sendCv.php
@@ -3,7 +3,8 @@
 function getSendCvByCustomer($db, $id){
     $sql = "SELECT id, consultant, date_envoi, candidat, client, poste "
             . "FROM cv_envoye "
-            . "WHERE client='".$id."' AND date_envoi >= date_sub(now(), interval 6 month)";
+            . "WHERE client='".$id."' AND date_envoi >= date_sub(now(), interval 6 month)"
+            . "ORDER BY date_envoi DESC";
 
     $r_cv = $db->prepare($sql);
     $r_cv->execute();
