0c17c8b8110126f9ee83cbde8d4b004262abf095
Christophe <Christophe@WORKSTATION.home>
2015-11-30 22:54:50 +0100

    21

--- facturation/simple_ndf.php
+++ facturation/simple_ndf.php
@@ -22,31 +22,36 @@ $month = $_GET['input_month'] ? htmlspecialchars($_GET['input_month']) : '';
 $year = $_GET['input_year'] ? htmlspecialchars($_GET['input_year']) : '';
 $ht = $_GET['input_ht'] ? htmlspecialchars($_GET['input_ht']) : '';
 $ttc = $_GET['input_ttc'] ? htmlspecialchars($_GET['input_ttc']) : '';
+$desc = $_GET['input_desc'] ? $_GET['input_desc'] : '';
 
 $sql = "SELECT ndf.id, ndf.description, ndf.ht_tot_amount, ndf.tva_tot_amount,"
-        . "ndf.ttc_tot_amount "
+        . "ndf.ttc_tot_amount, ndf.description "
         . "FROM notesfrais ndf ";
 
-if (!empty($month) || !empty($year) || !empty($id) || !empty($ht) || !empty($ttc))
+if (!empty($month) || !empty($year) || !empty($id) || !empty($ht) || !empty($ttc) || !empty($desc))
     $sql .= "WHERE ";
 if (!empty($month))
     $sql .= "mois = '" . $month . "'";
-if (!empty($month) && (!empty($year) || !empty($id) || !empty($ht) || !empty($ttc) ))
+if (!empty($month) && (!empty($year) || !empty($id) || !empty($ht) || !empty($ttc) || !empty($desc) ))
     $sql .= " AND ";
 if (!empty($year))
     $sql .= "annee = '" . $year . "'";
-if (!empty($year) && (!empty($id) || !empty($ht) || !empty($ttc) ))
+if (!empty($year) && (!empty($id) || !empty($ht) || !empty($ttc) || !empty($desc) ))
     $sql .= " AND ";
 if (!empty($id))
     $sql .= "id = '" . $id . "'";
-if (!empty($id) && (!empty($ht) || !empty($ttc) ))
+if (!empty($id) && (!empty($ht) || !empty($ttc) || !empty($desc)))
     $sql .= " AND ";
 if (!empty($ht))
     $sql .= "ht_tot_amount = '" . $ht . "'";
-if (!empty($ht) && (!empty($ttc) ))
+if (!empty($ht) && (!empty($ttc) || !empty($desc)))
     $sql .= " AND ";
 if (!empty($ttc))
     $sql .= "ttc_tot_amount = '" . $ttc . "'";
+if (!empty($ttc) && !empty($desc))
+    $sql .= " AND ";
+if (!empty($desc))
+    $sql .= "description like '%" . $desc . "%'";
 
 $sql .= " GROUP BY id ORDER BY id";
 //var_dump($sql);die;
@@ -56,7 +61,7 @@ $r_ndfs = $r_decaisse->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <body style="width:800px;margin: auto;">
-    <h1><?= count($r_ndfs) ?> notes de frais - <?= $_GET['input_month'] != '' ? $_GET['input_month'] : 'Aucun mois selectionne'; ?> <?= $_GET['input_year'] != '' ? $_GET['input_year'] : ''; ?></h1>
+    <h1><?= count($r_ndfs) ?> notes de frais - <?= $_GET['input_month'] != '' ? $_GET['input_month'] : 'Aucun mois selectionne'; ?>/<?= $_GET['input_year'] != '' ? $_GET['input_year'] : ''; ?></h1>
     <div>
         <table border="1" style="width:100%">
             <thead>
@@ -125,6 +130,71 @@ $r_ndfs = $r_decaisse->fetchAll(PDO::FETCH_OBJ);
                     endforeach;
                 }
                 ?>
+            </tbody>
+        </table>
+    </div>
+    <h1>Bilan</h1>
+    <div class="">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td>
+                        Total HT
+                    </td>
+                    <td style="text-align: right">
+                        <?= isset($total_ht_ndf) ? $total_ht_ndf : 0 ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total TVA 5,5%
+                    </td>
+                    <td style="text-align: right">
+                        <?= isset($total_tva_5_ndf) ? $total_tva_5_ndf : 0 ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total TVA 10%
+                    </td>
+                    <td style="text-align: right">
+                        <?= isset($total_tva_10_ndf) ? $total_tva_10_ndf : 0 ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total TVA 20%
+                    </td>
+                    <td style="text-align: right">
+                        <?= isset($total_tva_20_ndf) ? $total_tva_20_ndf : 0 ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total TVA inconnue
+                    </td>
+                    <td style="text-align: right">
+                        <?= isset($total_tva_unk_ndf) ? $total_tva_unk_ndf : 0 ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total TVA
+                    </td>
+                    <td style="text-align: right">
+                        <?=
+                        (isset($total_tva_0_ndf) ? $total_tva_0_ndf : 0) + (isset($total_tva_5_ndf) ? $total_tva_5_ndf : 0) + (isset($total_tva_10_ndf) ? $total_tva_10_ndf : 0) + (isset($total_tva_20_ndf) ? $total_tva_20_ndf : 0) + (isset($total_tva_unk_ndf) ? $total_tva_unk_ndf : 0)
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total TTC
+                    </td>
+                    <td style="text-align: right">
+                        <?= isset($total_ttc_ndf) ? $total_ttc_ndf : 0 ?>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
