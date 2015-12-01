7d444ec4cac52d6b814bc217ee416dc0e926f0b3
Christophe <Christophe@WORKSTATION>
2015-12-01 10:08:19 +0100

    21

--- facturation/ndf.php
+++ facturation/ndf.php
@@ -37,18 +37,18 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                         <select class="form-control" name="input_month" 
                                                 id="input_month">
                                             <option value=""></option>
-                                            <option value="janvier" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'janvier') echo "selected";?>>Janvier</option>
-                                            <option value="février" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'février') echo "selected";?>>Février</option>
-                                            <option value="mars" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'mars') echo "selected";?>>Mars</option>
-                                            <option value="avril" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'avril') echo "selected";?>>Avril</option>
-                                            <option value="mai" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'mai') echo "selected";?>>Mai</option>
-                                            <option value="juin" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'juin') echo "selected";?>>Juin</option>
-                                            <option value="juillet" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'juillet') echo "selected";?>>Juillet</option>
-                                            <option value="août" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'août') echo "selected";?>>Août</option>
-                                            <option value="septembre" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'septembre') echo "selected";?>>Septembre</option>
-                                            <option value="octobre" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'octobre') echo "selected";?>>Octobre</option>
-                                            <option value="novembre" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'novembre') echo "selected";?>>Novembre</option>
-                                            <option value="décembre" <?php if(isset($_GET['input_month']) && $_GET['input_month'] == 'décembre') echo "selected";?>>Décembre</option>
+                                            <option value="janvier" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'janvier') echo "selected"; ?>>Janvier</option>
+                                            <option value="février" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'février') echo "selected"; ?>>Février</option>
+                                            <option value="mars" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'mars') echo "selected"; ?>>Mars</option>
+                                            <option value="avril" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'avril') echo "selected"; ?>>Avril</option>
+                                            <option value="mai" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'mai') echo "selected"; ?>>Mai</option>
+                                            <option value="juin" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'juin') echo "selected"; ?>>Juin</option>
+                                            <option value="juillet" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'juillet') echo "selected"; ?>>Juillet</option>
+                                            <option value="août" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'août') echo "selected"; ?>>Août</option>
+                                            <option value="septembre" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'septembre') echo "selected"; ?>>Septembre</option>
+                                            <option value="octobre" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'octobre') echo "selected"; ?>>Octobre</option>
+                                            <option value="novembre" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'novembre') echo "selected"; ?>>Novembre</option>
+                                            <option value="décembre" <?php if (isset($_GET['input_month']) && $_GET['input_month'] == 'décembre') echo "selected"; ?>>Décembre</option>
                                         </select>
                                     </div>
                                 </div>
@@ -69,8 +69,8 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                     <label for="input_desc" class="col-lg-2 control-label">Description</label>
                                     <div class="col-lg-10">
                                         <textarea class="form-control" id="input_desc" name="input_desc" 
-                                               placeholder="Description"
-                                               ><?= isset($_GET['input_desc']) ? $_GET['input_desc'] : '' ?></textarea>
+                                                  placeholder="Description"
+                                                  ><?= isset($_GET['input_desc']) ? $_GET['input_desc'] : '' ?></textarea>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -89,17 +89,20 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                         <select class="form-control" name="input_year" 
                                                 id="input_year">
                                             <option value=""></option>
-                                            <?php 
-                                            for($i=2002 ; $i<=date('Y') ; $i++):
-                                            ?>
-                                                <option value="<?= $i ?>" 
-                                                    <?php 
-                                                    if(isset($_GET['input_year']) && $_GET['input_year'] == $i){echo "selected";}
-                                                    elseif(!isset($_GET['input_year']) && date('Y') == $i){echo "selected";}
-                                                    ?>><?= $i ?></option>
                                             <?php
-                                            endfor;
-                                            ?>
+                                            for ($i = 2002; $i <= date('Y'); $i++):
+                                                ?>
+                                                <option value="<?= $i ?>" 
+                                                <?php
+                                                if (isset($_GET['input_year']) && $_GET['input_year'] == $i) {
+                                                    echo "selected";
+                                                } elseif (!isset($_GET['input_year']) && date('Y') == $i) {
+                                                    echo "selected";
+                                                }
+                                                ?>><?= $i ?></option>
+                                                        <?php
+                                                    endfor;
+                                                    ?>
                                         </select>
                                     </div>
                                 </div>
@@ -109,13 +112,13 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                 </div>
 
                 <?php
-                if (!empty($_GET) && !isset($_GET['tab']) ) {
+                if (!empty($_GET) && !isset($_GET['tab'])) {
                     $r_ndfs = searchNdf($db);
 
                     if ($r_ndfs) {
                         //var_dump($r_ndfs);die;
                         ?>
-                        
+
                         <h1>Résultats - <?= count($r_ndfs) ?> notes de frais - <?= $_GET['input_month'] ?></h1>
                         <div class="jumbotron">
                             <table class="table table-striped table-hover ">
@@ -123,14 +126,13 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                     <tr>
                                         <th>ID</th>
                                         <th>Description</th>
-                                        <th>HT</th>
-                                        <th>TVA</th>
-                                        <th>TTC</th>
+                                        <th class="text-right">HT</th>
+                                        <th class="text-right">TVA</th>
+                                        <th class="text-right">TTC</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
-                                    
                                     $total_ht_ndf = 0;
                                     $total_tva_ndf = 0;
                                     $total_tva_0_ndf = 0;
@@ -139,7 +141,7 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                     $total_tva_20_ndf = 0;
                                     $total_tva_unk_ndf = 0;
                                     $total_ttc_ndf = 0;
-                                    
+
                                     foreach ($r_ndfs as $r_ndfs) {
                                         ?>
                                         <tr>
@@ -151,23 +153,23 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                             <td>
                                                 <?= $r_ndfs->description ?>
                                             </td>
-                                            <td>
-                                                <?= $r_ndfs->ht_tot_amount ?>
+                                            <td class="text-right">
+                                                <?= number_format($r_ndfs->ht_tot_amount, 2, '.', ' '); ?> €
                                             </td>
-                                            <td>
-                                                <?= $r_ndfs->tva_tot_amount ?>
+                                            <td class="text-right">
+                                                <?= number_format($r_ndfs->tva_tot_amount, 2, '.', ' '); ?> €
                                             </td>
-                                            <td>
-                                                <?= $r_ndfs->ttc_tot_amount ?>
+                                            <td class="text-right">
+                                                <?= number_format($r_ndfs->ttc_tot_amount, 2, '.', ' '); ?> €
                                             </td>
                                         </tr>
                                         <?php
                                         $total_ht_ndf += $r_ndfs->ht_tot_amount;
                                         $total_tva_ndf += $r_ndfs->tva_tot_amount;
                                         $total_ttc_ndf += $r_ndfs->ttc_tot_amount;
-                                        
+
                                         $ndfd = getAllNdfDByNdfId($db, $r_ndfs->id);
-                                        foreach($ndfd as $k => $v):
+                                        foreach ($ndfd as $k => $v):
                                             switch ($v->TVA_PERCENT):
                                                 case '5.50':
                                                     $total_tva_5_ndf += $v->TVA_AMOUNT;
@@ -203,52 +205,51 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                     <td>
                                         Total HT
                                     </td>
-                                    <td>
-                                        <?= isset($total_ht_ndf) ? $total_ht_ndf : 0 ?>
+                                    <td class="text-right">
+                                        <?= isset($total_ht_ndf) ? number_format($total_ht_ndf, 2, '.', ' ') : 0 ?> €
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         Total TVA 5,5%
                                     </td>
-                                    <td>
-                                        <?= isset($total_tva_5_ndf) ? $total_tva_5_ndf : 0 ?>
+                                    <td class="text-right">
+                                        <?= isset($total_tva_5_ndf) ? number_format($total_tva_5_ndf, 2, '.', ' ') : 0 ?> €
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         Total TVA 10%
                                     </td>
-                                    <td>
-                                        <?= isset($total_tva_10_ndf) ? $total_tva_10_ndf : 0 ?>
+                                    <td class="text-right">
+                                        <?= isset($total_tva_10_ndf) ? number_format($total_tva_10_ndf, 2, '.', ' ') : 0 ?> €
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         Total TVA 20%
                                     </td>
-                                    <td>
-                                        <?= isset($total_tva_20_ndf) ? $total_tva_20_ndf : 0 ?>
+                                    <td class="text-right">
+                                        <?= isset($total_tva_20_ndf) ? number_format($total_tva_20_ndf, 2, '.', ' ') : 0 ?> €
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         Total TVA inconnue
                                     </td>
-                                    <td>
-                                        <?= isset($total_tva_unk_ndf) ? $total_tva_unk_ndf : 0 ?>
+                                    <td class="text-right">
+                                        <?= isset($total_tva_unk_ndf) ? number_format($total_tva_unk_ndf, 2, '.', ' ') : 0 ?> €
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         Total TVA
                                     </td>
-                                    <td>
-                                        <?= (isset($total_tva_0_ndf) ? $total_tva_0_ndf : 0) 
-                                            + (isset($total_tva_5_ndf) ? $total_tva_5_ndf : 0) 
-                                            + (isset($total_tva_10_ndf) ? $total_tva_10_ndf : 0) 
-                                            + (isset($total_tva_20_ndf) ? $total_tva_20_ndf : 0) 
-                                            + (isset($total_tva_unk_ndf) ? $total_tva_unk_ndf : 0) 
+                                    <td class="text-right">
+                                        <?php
+                                        $totalTvaBilan = (isset($total_tva_0_ndf) ? $total_tva_0_ndf : 0) + (isset($total_tva_5_ndf) ? $total_tva_5_ndf : 0) + (isset($total_tva_10_ndf) ? $total_tva_10_ndf : 0) + (isset($total_tva_20_ndf) ? $total_tva_20_ndf : 0) + (isset($total_tva_unk_ndf) ? $total_tva_unk_ndf : 0);
+
+                                        echo number_format($totalTvaBilan, 2, '.', ' ') . ' €';
                                         ?>
                                     </td>
                                 </tr>
@@ -256,8 +257,8 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                     <td>
                                         Total TTC
                                     </td>
-                                    <td>
-                                        <?= isset($total_ttc_ndf) ? $total_ttc_ndf : 0 ?>
+                                    <td class="text-right">
+                                        <?= isset($total_ttc_ndf) ? number_format($total_ttc_ndf, 2, '.', ' ') : 0 ?> €
                                     </td>
                                 </tr>
                             </tbody>
@@ -303,11 +304,11 @@ if (!($_SESSION['user']['type'] == "ADMIN" || $_SESSION['user']['type'] == "SUPE
                                         <select class="form-control" name="input_year" 
                                                 id="input_year">
                                             <option value=""></option>
-                                            <?php 
-                                            for($i=2002 ; $i<=date('Y') ; $i++):
-                                            ?>
-                                                <option value="<?= $i ?>" <?php if(date('Y') == $i) echo "selected"; ?>><?= $i ?></option>
                                             <?php
+                                            for ($i = 2002; $i <= date('Y'); $i++):
+                                                ?>
+                                                <option value="<?= $i ?>" <?php if (date('Y') == $i) echo "selected"; ?>><?= $i ?></option>
+                                                <?php
                                             endfor;
                                             ?>
                                         </select>
